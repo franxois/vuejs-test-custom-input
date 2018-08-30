7ad2396a071709a8a397f1e5ec38fc9f2e130583
François Rieucau <franxoois@gmail.com>
2018-08-30 23:18:03 +0200

    feat(custom input): Turn textarea into custom input

---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <mytextarea v-model="hello" />
+    {{ hello }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import mytextarea from "./MyTextarea";
+
+@Component({
+  components: {
+    mytextarea
+  }
+})
+export default class Main extends Vue {
+  @Prop() private msg!: string;
+  hello: string = "Hello World";
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/MyTextarea.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <textarea :value="myData" @input="updateContent"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Model, Emit } from "vue-property-decorator";
+import { EventEmitter } from "events";
+
+@Component
+export default class HelloWorld extends Vue {
+  updateContent(event: any) {
+    this.$emit("myDataChanged", event.target.value);
+  }
+
+  @Model("myDataChanged", { type: String })
+  myData!: String;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
